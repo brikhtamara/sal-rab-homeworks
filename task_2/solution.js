6213fca85d55c03c048985c43802e53efbf8a23d
brikhtamara <brihova@rambler.ru>
2021-12-14 01:27:28 +0300

    task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -29,10 +29,10 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     let discoumtSum;// создайте переменную discountSum
-
-    if (productsSum >= discountMinSum)// если productsSum больше или равно discountMinSum,
-    {console.log(discountSum = productsSum / 100 * discountPart)}// то присвойте discountSum значение discountPart процентов от productsSum,
-    else {console.log(discountSum = 0)};// иначе присвойте discountSum значение 0
+    let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0;
+    // если productsSum больше или равно discountMinSum,
+    // то присвойте discountSum значение discountPart процентов от productsSum,
+    // иначе присвойте discountSum значение 0
 
     // Конец решения задания №2.2.
 
